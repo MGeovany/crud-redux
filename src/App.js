36ba5b2f2fb1945d7fbe7434bff54544172ea332
Geovany <marlongeo1999@gmail.com>
2022-06-23 20:55:53 -0600

    🥨 configured prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import "./App.css";
+import './App.css'
 
 function App() {
-  return <div className="App">Hello Redux 💜</div>;
+  const body = 3 * Math.PI * Math.pow(10, 2)
+
+  return (
+    <div className='App'>
+      Hello Redux 💜
+      <p>The body of the earth is {body.toFixed(2)}km²</p>
+    </div>
+  )
 }
 
-export default App;
+export default App
